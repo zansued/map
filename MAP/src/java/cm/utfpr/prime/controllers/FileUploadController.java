10f50f2b28ab4355f640c082cfff7c9c6abe38a0
Guilherme <zan.sued@gmail.com>
2013-05-06 20:00:58 -0300

    Criação de um novo projeto, pois o antigo não dava certo. Criação do MAP

---
+++ MAP/src/java/cm/utfpr/prime/controllers/FileUploadController.java
@@ -0,0 +1,15 @@
+package cm.utfpr.prime.controllers;
+
+  
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;  
+import org.primefaces.event.FileUploadEvent;
+  
+ 
+public class FileUploadController {  
+  
+    public void handleFileUpload(FileUploadEvent event) {  
+        FacesMessage msg = new FacesMessage("Succesful", event.getFile().getFileName() + " is uploaded.");  
+        FacesContext.getCurrentInstance().addMessage(null, msg);  
+    }  
+}  
---
+++ MAP/src/java/cm/utfpr/prime/controllers/ScheduleController.java
@@ -0,0 +1,41 @@
+package cm.utfpr.prime.controllers;
+
+
+import java.io.Serializable;
+import java.util.Calendar;
+import java.util.Date;
+import org.primefaces.model.DefaultScheduleEvent;
+import org.primefaces.model.LazyScheduleModel;
+import org.primefaces.model.ScheduleModel;
+
+public class ScheduleController implements Serializable {
+
+    private ScheduleModel lazyEventModel;
+
+    public ScheduleController() {
+        lazyEventModel = new LazyScheduleModel() {
+            @Override
+            public void loadEvents(Date start, Date end) {
+                clear();
+
+                Date random = getRandomDate(start);
+                addEvent(new DefaultScheduleEvent("Lazy Event 1", random, random));
+
+                random = getRandomDate(start);
+                addEvent(new DefaultScheduleEvent("Lazy Event 2", random, random));
+            }
+        };
+    }
+
+    public Date getRandomDate(Date base) {
+        Calendar date = Calendar.getInstance();
+        date.setTime(base);
+        date.add(Calendar.DATE, ((int) (Math.random() * 30)) + 1);	//set random day of month
+
+        return date.getTime();
+    }
+
+    public ScheduleModel getLazyEventModel() {
+        return lazyEventModel;
+    }
+}
